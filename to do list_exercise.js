3577402b064309ba4fa674bd903450789b03b122
isaac-michel224 <84110054+isaac-michel224@users.noreply.github.com>
2021-07-14 20:57:03 -0400

    Add files via upload

---
+++ to do list_exercise.js
@@ -0,0 +1,112 @@
+/*To Do List
+Let's create a simple to do application 
+
+Show an unordered list of to do's 
+Show an input to enter a new to do 
+Show a button to add a to  do. When the button is clicked: 
+The text from the input box is used to add a list item to the bottom of the list 
+The text from the input box is cleared out.
+When the user clicks on a list item, it is removed
+
+Extra Credit: - When a list item is clicked, cross it out, then remove it after 1 second.
+**Submit: A link to your github repo for the project as well as screenshots of the project working in the browser */
+
+// Create a "close" button and append it to each list item
+
+window.onload = () => {
+	const form1 = document.querySelector("#addForm");
+
+	let items = document.getElementById("items");
+	let submit = document.getElementById("submit");
+
+	let editItem = null;
+
+	form1.addEventListener("submit", addItem);
+	items.addEventListener("click", removeItem);
+};
+
+function addItem(e) {
+	e.preventDefault();
+
+	if (submit.value != "Submit") {
+		console.log("Hello");
+
+		editItem.target.parentNode.childNodes[0].data
+			= document.getElementById("item").value;
+
+		submit.value = "Submit";
+		document.getElementById("item").value = "";
+
+		document.getElementById("lblsuccess").innerHTML
+			= "Text edited successfully";
+
+		document.getElementById("lblsuccess")
+						.style.display = "block";
+
+		setTimeout(function() {
+			document.getElementById("lblsuccess")
+							.style.display = "none";
+		}, 3000);
+
+		return false;
+	}
+
+	let newItem = document.getElementById("item").value;
+	if (newItem.trim() == "" || newItem.trim() == null)
+		return false;
+	else
+		document.getElementById("item").value = "";
+
+	let li = document.createElement("li");
+	li.className = "list-group-item";
+
+	let deleteButton = document.createElement("button");
+
+	deleteButton.className =
+		"btn-danger btn btn-sm float-right delete";
+
+	deleteButton.appendChild(document.createTextNode("Delete"));
+
+	let editButton = document.createElement("button");
+
+	editButton.className =
+			"btn-success btn btn-sm float-right edit";
+
+	editButton.appendChild(document.createTextNode("Edit"));
+
+	li.appendChild(document.createTextNode(newItem));
+	li.appendChild(deleteButton);
+	li.appendChild(editButton);
+
+	items.appendChild(li);
+}
+
+function removeItem(e) {
+	e.preventDefault();
+	if (e.target.classList.contains("delete")) {
+		if (confirm("Are you Sure?")) {
+			let li = e.target.parentNode;
+			items.removeChild(li);
+			document.getElementById("lblsuccess").innerHTML
+				= "Text deleted successfully";
+
+			document.getElementById("lblsuccess")
+						.style.display = "block";
+
+			setTimeout(function() {
+				document.getElementById("lblsuccess")
+						.style.display = "none";
+			}, 3000);
+		}
+	}
+	if (e.target.classList.contains("edit")) {
+		document.getElementById("item").value =
+			e.target.parentNode.childNodes[0].data;
+		submit.value = "EDIT";
+		editItem = e;
+	}
+}
+
+function toggleButton(ref, btnID) {
+	document.getElementById(btnID).disabled = false;
+}
